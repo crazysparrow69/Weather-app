f181fd0ff452fc6e10375e2a42ddf839af3c6f60
Denys <denissytnikmacho@gmail.com>
2022-08-10 13:31:51 +0300

    finalized the project

--- app.js
+++ app.js
@@ -61,23 +61,82 @@ const weather = {
         temp_max:     Math.floor(data.main.temp_max),
         wind:         data.wind.speed,
         pressure:     data.main.pressure,
-        humidity:     data.main.humidity
+        humidity:     data.main.humidity,
       }
+
       return indicators;
     }).then(indicators => this.showWeather(indicators));
   },
 
+  getDate: function() {
+    const options = { weekday: 'long'};
+    const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+    const data = new Date();
+    const date = {
+      day: data.getDate(),
+      weekDay: new Intl.DateTimeFormat('ru-RU', options).format(data),
+      month: data.getMonth(),
+      hours: data.getHours(),
+      minutes: data.getMinutes()
+    };
+
+    return `${date.hours}:${date.minutes} - ${date.day} ${months[date.month]}, ${date.weekDay}`;
+  },
+
+  changeBackground: function() {
+    const cities = {
+      Belgorod: '/img/cities/belgorod.jpg',
+      Kyiv: '/img/cities/kyiv.jpg',
+      London: '/img/cities/london.jpg',
+      Moscow: '/img/cities/moscow.jpg',
+      Paris: '/img/cities/paris.jpg',
+      Tokyo: '/img/cities/tokyo.jpg',
+      Toronto: '/img/cities/toronto.jpg'
+    };
+
+    const city = this.selectedCity();
+    
+    if (city === 'Belgorod') this.DOM.title.img.src = '/img/weather/cotton.jpg';
+
+    for (const key in cities) {
+      if (city === key) {
+        const link = `${cities[key]}`;
+        document.body.style.backgroundImage = `url(${link})`;
+      }
+    };
+  },
+
+  changeIcon: function(condition) {
+    const conditions = {
+      Clear: '/img/weather/clear.jpg',
+      Clouds: '/img/weather/clouds.jpg',
+      Rain: '/img/weather/rain.jpg',
+      Snow: '/img/weather/snow.jpg',
+      Default: '/img/weather/default.jpg'
+    };
+
+    this.DOM.title.img.src = conditions.Default;
+
+    for (const key in conditions) {
+      if (condition === key) {
+        this.DOM.title.img.src = conditions[key];
+      }
+    };
+  },
+
   showWeather: function(indicators) {
-    this.DOM.title.city.innerHTML = `Погода в ${indicators.city} сейчас`;
-    this.DOM.title.date.innerHTML = indicators.city;
-    this.DOM.title.img.src.innerHTML = '/img/weather/clouds.jpg';
-    this.DOM.temperature.current.innerHTML = indicators.current_temp;
-    this.DOM.temperature.feels_like.innerHTML = indicators.feels_like;
-    this.DOM.temperature.min_temp.innerHTML = indicators.temp_min;
-    this.DOM.temperature.max_temp.innerHTML = indicators.temp_max;
-    this.DOM.other.wind.innerHTML = indicators.wind;
-    this.DOM.other.pressure.innerHTML = indicators.pressure;
-    this.DOM.other.humidity.innerHTML = indicators.humidity;
+    this.DOM.title.city.innerHTML =             `Погода в ${indicators.city} сейчас`;
+    this.DOM.title.date.innerHTML =             this.getDate();
+    this.DOM.title.img.src.innerHTML =          '/img/weather/clouds.jpg';
+    this.DOM.temperature.current.innerHTML =    `+${indicators.current_temp}°`;
+    this.DOM.temperature.feels_like.innerHTML = `+${indicators.feels_like}°`;
+    this.DOM.temperature.min_temp.innerHTML =   `+${indicators.temp_min}°`;
+    this.DOM.temperature.max_temp.innerHTML =   `+${indicators.temp_max}°`;
+    this.DOM.other.wind.innerHTML =             `${indicators.wind} км/ч`;
+    this.DOM.other.pressure.innerHTML =         `${indicators.pressure} мм р.с.`;
+    this.DOM.other.humidity.innerHTML =         `${indicators.humidity}%`;
+    this.changeIcon(indicators.current);
+    this.changeBackground();
   }
 }
 
